5f10e273c78b5fb01f3ce19c9293cdeebc4ff853
peanattha <nuttapong.tho@kkumail.com>
2023-03-08 23:27:48 +0700

    error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>404 - {{ config('app.name') }}</title>
+
+    <link rel="icon" type="image/svg" href="{{ asset('images/Logo.svg') }}" />
+    </head>
+
+    <body>
+        <div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="text-center">
+                <h1 class="display-1 fw-bold">404</h1>
+                <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+                <p class="lead">
+                    The page you’re looking for doesn’t exist.
+                  </p>
+                <a href="{{route('home')}}" class="btn btn-success">Go Home</a>
+            </div>
+        </div>
+    </body>
+
+
+</html>
